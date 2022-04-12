03cbc7d2611e80d5e62923ca85143b8886f2a398
Jose <josecarvajalhilario@gmail.com>
2022-04-12 08:37:10 +0200

    Allow to have dependencies in service state

--- jcloud-benchmark/src/main/java/io/jcloud/core/ServiceState.java
+++ jcloud-benchmark/src/main/java/io/jcloud/core/ServiceState.java
@@ -1,5 +1,7 @@
 package io.jcloud.core;
 
+import java.util.stream.Stream;
+
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
@@ -12,9 +14,11 @@ import io.jcloud.api.Service;
 public class ServiceState<T extends Service> {
 
     private final T service;
+    private final Service[] dependencies;
 
-    public ServiceState(T service) {
+    public ServiceState(T service, Service... dependencies) {
         this.service = service;
+        this.dependencies = dependencies;
         if (service.isRunning()) {
             throw new IllegalStateException(
                     "Service is already running! You need to declare services using `.setAutoStart(false)`");
@@ -27,11 +31,13 @@ public class ServiceState<T extends Service> {
 
     @Setup(Level.Trial)
     public void doSetup() {
+        Stream.of(dependencies).forEach(Service::start);
         service.start();
     }
 
     @TearDown(Level.Trial)
     public void doTearDown() {
         service.stop();
+        Stream.of(dependencies).forEach(Service::stop);
     }
 }
