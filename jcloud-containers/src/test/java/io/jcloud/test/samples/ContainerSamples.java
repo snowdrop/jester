d6f571b83a53aabfc7496edb9653d1e16408f7a1
Jose <josecarvajalhilario@gmail.com>
2022-04-12 09:36:30 +0200

    Add json capabilities to http service

--- jcloud-containers/src/test/java/io/jcloud/test/samples/ContainerSamples.java
+++ jcloud-containers/src/test/java/io/jcloud/test/samples/ContainerSamples.java
@@ -6,12 +6,15 @@ package io.jcloud.test.samples;
  */
 public final class ContainerSamples {
     public static final String QUARKUS_REST_IMAGE = "${sample.quarkus-rest.image:server.io/test/quarkus-rest:latest}";
+    public static final String QUARKUS_JSON_IMAGE = "${sample.quarkus-json.image:server.io/test/quarkus-json:latest}";
     public static final String QUARKUS_REST_LOCATION = "images/quarkus-rest";
 
     public static final int SAMPLES_DEFAULT_PORT = 8080;
     public static final String QUARKUS_STARTUP_EXPECTED_LOG = "Installed features";
     public static final String SAMPLES_DEFAULT_REST_PATH = "/hello";
+    public static final String SAMPLES_DEFAULT_JSON_PATH = "/hello";
     public static final String SAMPLES_DEFAULT_REST_PATH_OUTPUT = "Hello Samples";
+    public static final String SAMPLES_DEFAULT_JSON_PATH_OUTPUT = "Hello Samples";
 
     private ContainerSamples() {
 
