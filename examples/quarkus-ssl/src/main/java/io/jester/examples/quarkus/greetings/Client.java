eeebf55f888e3d3a2974e4598b3d71962225c830
Jose <josecarvajalhilario@gmail.com>
2022-09-14 12:42:36 +0200

    Improve the Quarkus SSL example

---
+++ examples/quarkus-ssl/src/main/java/io/jester/examples/quarkus/greetings/Client.java
@@ -0,0 +1,16 @@
+package io.jester.examples.quarkus.greetings;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(configKey = "client")
+public interface Client {
+    @GET
+    @Path("/greeting")
+    @Produces(MediaType.TEXT_PLAIN)
+    String helloFromClient();
+}
