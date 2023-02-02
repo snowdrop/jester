0e8163e9ef39591230330c211863375281df6bf8
Antonio Costa <a.costa@trikorasolutions.com>
2023-02-02 14:22:50 +0100

    feat: OpenShift support

---
+++ jester-containers/src/test/java/io/jester/test/OpenShiftServiceLifecycleIT.java
@@ -0,0 +1,32 @@
+package io.jester.test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.jester.api.RunOnOpenShift;
+import io.jester.api.clients.OpenshiftClient;
+
+@RunOnOpenShift
+public class OpenShiftServiceLifecycleIT extends ServiceLifecycleIT {
+
+    @Inject
+    static OpenshiftClient clientAsStaticInstance;
+
+    @Inject
+    static OpenShiftClient fabric8OpenShiftClient;
+
+    @Test
+    public void shouldInjectOpenShiftClientsAsStaticInstance() {
+        assertNotNull(clientAsStaticInstance);
+        assertNotNull(fabric8OpenShiftClient);
+    }
+
+    @Test
+    public void shouldInjectOpenShiftClientAsField(OpenshiftClient clientAsField) {
+        assertNotNull(clientAsField);
+    }
+}
