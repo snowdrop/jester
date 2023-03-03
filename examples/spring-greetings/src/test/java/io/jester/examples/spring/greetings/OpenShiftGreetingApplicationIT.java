56554ceab27dcb5cb72a086a0824ad6b28115736
Jose <josecarvajalhilario@gmail.com>
2023-03-03 10:26:03 +0100

    Support of OpenShift deployments for Spring Boot apps
    Fix https://github.com/Sgitario/jester/issues/202

---
+++ examples/spring-greetings/src/test/java/io/jester/examples/spring/greetings/OpenShiftGreetingApplicationIT.java
@@ -0,0 +1,7 @@
+package io.jester.examples.spring.greetings;
+
+import io.jester.api.RunOnOpenShift;
+
+@RunOnOpenShift
+public class OpenShiftGreetingApplicationIT extends GreetingApplicationIT {
+}
